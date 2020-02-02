54cd2295eb7852094c85384d20dd2e1f8bec66b2
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-02 15:58:55 -0500

    set up framework for DELETE route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -50,12 +50,12 @@ module.exports = function(app) {
   });
 
   // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+// DELETE
 
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
+  app.delete("/api/notes/:id", function(req, res) {
+    // Sets length of note to 0
+    notes.length = 0;
+    var id = req.params.id;
   //  waitListData.length = 0;
 
     res.json({ ok: true });
