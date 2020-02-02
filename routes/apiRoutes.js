b777722770aa1b30dbbad96841dbfcba26a64b27
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-02 14:51:10 -0500

    commented out the class activity's load data variables, and changed the GET to api/notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,10 +3,10 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
-
+/*
 var tableData = require("../data/tableData");
 var waitListData = require("../data/waitinglistData");
-
+*/
 
 // ===============================================================================
 // ROUTING
@@ -19,14 +19,14 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+  app.get("/api/notes", function(req, res) {
+  //  res.json(tableData);
   });
-
+/*
   app.get("/api/waitlist", function(req, res) {
     res.json(waitListData);
   });
-
+*/
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
