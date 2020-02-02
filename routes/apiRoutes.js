6463e228fce74dd6182e01fcf34ce6416aea515c
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-02 15:38:10 -0500

    changed load data and routing variables to notes and changed file to require

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,10 +3,10 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
-/*
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
-*/
+
+var notes = require("../db/db.json");
+// var waitListData = require("../data/waitinglistData");
+
 
 // ===============================================================================
 // ROUTING
@@ -20,7 +20,7 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/api/notes", function(req, res) {
-  //  res.json(tableData);
+  res.json(notes);
   });
 /*
   app.get("/api/waitlist", function(req, res) {
