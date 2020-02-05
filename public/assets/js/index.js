456782118289fd5ac8c05e6caf8f034223838a1f
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-05 14:23:07 -0500

    connected stylesheets and added buttons

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,9 +1,9 @@
 fs = require('fs');
 
-var $noteTitle = $(".note-title");
-var $noteText = $(".note-textarea");
-var $saveNoteBtn = $(".save-note");
-var $newNoteBtn = $(".new-note");
+var $noteTitle = $("#note-title");
+var $noteText = $("#textarea");
+var $saveNoteBtn = $("#save");
+var $newNoteBtn = $("#new");
 var $noteList = $(".list-container .list-group");
 
 // activeNote is used to keep track of the note in the textarea
