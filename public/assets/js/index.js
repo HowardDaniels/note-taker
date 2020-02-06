4f6ead3b2ed28521e46079a27671a08d582dfb5f
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-06 08:18:07 -0500

    reverted the IDs and classes to old code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 // revert IDs and classes to old code
-var $noteTitle = $("#note-title");
-var $noteText = $("#textarea");
-var $saveNoteBtn = $("#save");
-var $newNoteBtn = $("#new");
+var $noteTitle = $(".note-title");
+var $noteText = $(".note-textarea");
+var $saveNoteBtn = $(".save-note");
+var $newNoteBtn = $(".new-note");
 var $noteList = $(".list-container .list-group");
 
 // activeNote is used to keep track of the note in the textarea
